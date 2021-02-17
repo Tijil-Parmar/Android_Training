768f7305ec344ecc44b7d94f01e79ed35d5b9f34
TSP57081 <36893740+TSP57081@users.noreply.github.com>
2021-02-17 19:49:48 +0530

    Notification Intent

--- app/src/main/java/com/example/StudyGoals/NotificationBroadcast.java
+++ app/src/main/java/com/example/StudyGoals/NotificationBroadcast.java
@@ -1,7 +1,9 @@
 package com.example.StudyGoals;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,10 +18,20 @@ public class NotificationBroadcast extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent intentToRepeat = new Intent(context, MainActivity.class);
+        //set flag to restart/relaunch the app
+        intentToRepeat.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        //Pending intent to handle launch of Activity in intent above
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, AlarmManager.RTC_WAKEUP, intentToRepeat, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
         NotificationCompat.Builder notification = new NotificationCompat.Builder(context, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle("Trin Trin!")
                 .setContentText("It's time to study.Let's get started :)")
+                .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE);
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
